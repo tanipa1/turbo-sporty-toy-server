783fa407ce236014fb9deede5b4df6a461e33786
tanipa1 <tania35.291@gmail.com>
2023-05-20 13:57:20 +0600

    Insert one Operation

--- index.js
+++ index.js
@@ -34,6 +34,12 @@ async function run() {
       res.send(result);
     })
 
+    app.post('/toys', async (req, res) => {
+      const toys = req.body;
+      const result = await toyCollection.insertOne(toys);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
