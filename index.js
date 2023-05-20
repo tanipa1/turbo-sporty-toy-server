279abd0ce35ee6ce6f26c8d74d02cecd51a35b83
tanipa1 <tania35.291@gmail.com>
2023-05-20 19:08:20 +0600

    Signle Data find

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -34,11 +34,19 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/toys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    })
+
     app.post('/toys', async (req, res) => {
       const toys = req.body;
       const result = await toyCollection.insertOne(toys);
       res.send(result);
     })
+    
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
